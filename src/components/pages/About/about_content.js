080259b5dd1283eb9f052ffa26d2e7a57cc32c2f
James Formica <james.formica@gmail.com>
2018-06-01 14:35:33 +1000

    chore: fix redirects and add a bit of content

--- src/components/pages/About/about_content.js
+++ src/components/pages/About/about_content.js
@@ -12,4 +12,8 @@ My goal is to run fun and interactive sessions where the whole class builds the 
 ## What does code3.5 mean / stand for?
 
 Naming is hard! As you learn to code, you will discover how difficult it can be to name things. Most of the web is now built on 2 standard technologies: CSS3 and HTML5. I grabbed those numbers, put a dot between them and shoved the word 'code' in front! It may not be the most catchy name ever, but it is somewhat relevant :)
+
+## Who am I?
+
+Hi there! I'm James Formica, I'm a software developer currently working at [realestate.com.au](http://www.realestate.com.au). I'm technically a UI Developer which pretty much means I specialise in making the website look good and behave well.
 `
