41c6a1a9547335a3e4e3512616f60328feb25994
James Formica <james.formica@gmail.com>
2018-06-06 08:23:24 +1000

    feat: update about section content

--- src/components/pages/About/about_content.js
+++ src/components/pages/About/about_content.js
@@ -5,15 +5,28 @@ In short, code3.5 is a community club that aims to inspire younglings to create 
 There is a common misconception that to write code you need to be a genius. Well, I am living proof that
 anyone can learn to code!
 
-Honestly, code3.5 doesn't really exist yet... YET! But it will!
+## Where is code3.5?
 
-My goal is to run fun and interactive sessions where the whole class builds the website together and decides on the outcome. I am based out in Doreen Melbourne VIC and am hoping to utilise the community space to run the class.
+Currently the only location that code3.5 is running is in a Melbourne north-east suburb called Doreen. It is being run out of the Orchard Road Community Centre which is at: **Orchard Rd & Patience Ave, Doreen VIC 3754**.
+
+## How much does it cost?
+
+$0.00! code3.5 is absolutely free and I intend to keep it that way for as long as possible.
+
+## Who am I?
+
+Hi there! I'm James Formica, I'm a software developer currently working at [realestate.com.au](http://www.realestate.com.au). I'm technically a UI Developer which pretty much means I specialise in making the website look good and behave well.
+
+## Why do this?
+
+I remember learning to code at the beginning and I also remember it being rather boring until I could build my own stuff. My goal is to run fun and interactive sessions where the whole class builds the website together and decides on the finished product.
+
+## Will there be homework?
+
+Nope.
 
 ## What does code3.5 mean / stand for?
 
 Naming is hard! As you learn to code, you will discover how difficult it can be to name things. Most of the web is now built on 2 standard technologies: CSS3 and HTML5. I grabbed those numbers, put a dot between them and shoved the word 'code' in front! It may not be the most catchy name ever, but it is somewhat relevant :)
 
-## Who am I?
-
-Hi there! I'm James Formica, I'm a software developer currently working at [realestate.com.au](http://www.realestate.com.au). I'm technically a UI Developer which pretty much means I specialise in making the website look good and behave well.
 `
